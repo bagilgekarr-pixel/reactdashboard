bbf7f35b082cf24feb159cbdc30a4cb29aafa8a8
Rohit <bagilgekarr@gmail.com>
2026-09-09 14:55:32 +0530

    updated homepage

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Home = () => {
+
+  const getAlert = () =>{
+    alert("Button qu Click kiya ");
+  }
+  const [number,setNumber] = useState(200)
+
+  const increment = () =>{
+    setNumber(number+1)
+  }
+
+    const Decrement = () =>{
+    setNumber(number-1)
+  }
+
   return (
    <>
-   <h1>This is Home Page</h1>
+  
+   <button onClick={getAlert}>Click Me</button>
+   
+   <button onClick={increment}>Increment</button>
+
+   <button onClick={Decrement}>Decrement</button>
+
+     
+     
+     <h1>Number : {number}</h1>
+   
    
    </>
   )
